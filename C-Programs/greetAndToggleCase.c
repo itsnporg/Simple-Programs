2fb5656a73f0407bca1b3d07d0f18127533df06c
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-10-01 14:46:56 +0530

    docs : c program to take user input and toggle case

---
+++ C-Programs/greetAndToggleCase.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+const int ASCII_DIFFERENCE = 32;
+
+int main (void)
+{
+    int UPPERCASE;
+
+  string name = get_string("Input: ");
+  printf("Output: ");
+
+  for(int i =0; i < name[i] != '\0' ; i++)
+  {
+    // checking lowercase by comparing ASCII
+    if(name[i] >= 'a' && name[i] <= 'z')
+    {
+         printf("%c" , name[i]- ASCII_DIFFERENCE);
+    }
+    
+   
+  }
+ 
+}
