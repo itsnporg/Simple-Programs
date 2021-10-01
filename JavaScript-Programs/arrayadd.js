b72917ea4caef50048640c9f2807935f62d41868
B0s0 <80257174+B0s0@users.noreply.github.com>
2021-10-01 15:48:19 +0545

    Update arrayadd.js

--- JavaScript-Programs/arrayadd.js
+++ JavaScript-Programs/arrayadd.js
@@ -1,31 +1,31 @@
 //adds elements of two arrays simultaneously
 
 
-  const carry=[]; //carry array 
-  const sum=[]; //array to push sums to
+  const carry=[];
+  const sum=[];
 
-  //checking for the array with greater size 
+  //checking for the array with greater length 
   if (l1.length > l2.length) {
-   var forLength = l1.length; 
+   let forLength = l1.length; 
   }
   else if (l1.length < l2.length) {
-      var forLength = l2.length;
+    let forLength = l2.length;
   }
   else {
-      var forLength = l1.length;
+    let forLength = l1.length;
   }
 
-  //for loop that goes through simultaneous elements of both arrays
-  for (var i = 0; i < forLength; i++) {
+  //for loop that goes through all the elements of both arrays simultaneously
+  for (let i = 0; i < forLength; i++) {
 
-      //checking if elements are the first or not and assigning value of carry from the array
+      //checking if index of the current element is 0 or not.
       if (i === 0) {
           carrynum = 0
       } else {
           carrynum = carry[i-1];
       }
 
-      //checking if both or one of the elements are present and also if the element is 0
+      //checking if current element of at least one array is present or if it is 0
       if (l1[i] && l2[i]){ 
           sumNum = l1[i] + l2[i] + carrynum
       } else if (l1[i] && !l2[i]) {
@@ -37,8 +37,8 @@
           sumNum = 0;
       }
 
-      // basically pushes the sum and carry to their arrays
-      var sumArr = String(sumNum).split("").map((sumNum)=>{
+      // appending to sum and carry array
+      let sumArr = String(sumNum).split("").map((sumNum)=>{
           return Number(sumNum)
       })
       if (sumArr.length === 2) {
