3a04f11e7f6a50cbe0903d35264d8bba7089c368
Deshan-Chathuranga <deshanc321@gmail.com>
2021-10-02 16:46:56 +0530

    Removed the unnecessary spaces of the ADTFraction file

--- Java-Programs/ADTFraction.java
+++ Java-Programs/ADTFraction.java
@@ -2,13 +2,10 @@ public class ADTFraction{
     private int n; //numerator
     private int d; //denomenator
 
-
     public ADTFraction() {//default constructor
         this.n = 0;
         this.d = 1;
     }
-
-
     public ADTFraction(int a, int b) {//parameter constructor
         if (b != 0) {
             this.n = a;
@@ -16,11 +13,8 @@ public class ADTFraction{
         } else {
             this.n = 0;
             this.d = 1;
-            System.out.println("Denominator cannot be zero. default values are assigned.");
-        }
+            System.out.println("Denominator cannot be zero. default values are assigned."); }
     }
-
-
     public void set(int a, int b) {//set numerator and denomenator
         if (b != 0) {
             this.n = a;
@@ -28,11 +22,8 @@ public class ADTFraction{
         } else {
             this.n = 0;
             this.d = 1;
-            System.out.println("Denominator cannot be zero. default values are assigned.");
-        }
+            System.out.println("Denominator cannot be zero. default values are assigned."); }
     }
-
-
     public ADTFraction plus(ADTFraction x) {//add two fractions this=3/5 and x=7/8
         int num, den;
         num = x.d * this.n + x.n * this.d;
@@ -40,8 +31,6 @@ public class ADTFraction{
         ADTFraction f1 = new ADTFraction(num, den);
         return f1;
     }
-
-
     public ADTFraction times(int a) {//multiply fraction by a number
         int num, den;
         num=this.n*a;
@@ -49,32 +38,21 @@ public class ADTFraction{
         ADTFraction f1=new ADTFraction(num,den);
         return f1;
     }
-
-
     public ADTFraction times(ADTFraction x) {//multiply two fractions
         int num ,den;
         num= this.n+ x.n;
         den= this.d+ x.d;
-
         ADTFraction f1= new ADTFraction(num,den);
         return f1;
-
     }
-
-
     public ADTFraction reciprocal() {//reciprocal of a fraction upside down
         ADTFraction f1=new ADTFraction(this.d,this.n);
         return f1;
     }
-
-
     public float value() {//numerical value of a fraction
         return (float)this.n/this.d;
     }
-
-
     public void display() {//display the fraction in the format n/d
         System.out.println("Fraction equals to "+this.n+ "/" + this.d);
     }
-
 }
