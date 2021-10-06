8a39fc45a942922a3703405d4fa3f3bd4d05555a
vijoyaliyan <92007603+vijoyaliyan@users.noreply.github.com>
2021-10-06 11:00:58 +0530

    Create DeterminantMatrix.java
    Program to find the determinant of a matrix.

---
+++ Java-Programs/DeterminantMatrix.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DeterminantOfMatrix {
+	private static Scanner sc;
+	
+	public static void main(String[] args) {
+		int[][] arr = new int[2][2];
+		
+		int i, j, determinant = 0;
+		
+		sc= new Scanner(System.in);
+		
+		System.out.println("\n Please Enter the Matrix Items :  ");
+		for(i = 0; i < 2; i++) {
+			for(j = 0; j < 2; j++) {
+				arr[i][j] = sc.nextInt();
+			}		
+		}
+		
+		determinant = (arr[0][0] * arr[1][1]) - (arr[0][1] * arr[1][0]);
+
+		System.out.println("The Determinant of 2 * 2 Matrix = " + determinant );
+		
+	}
+}
