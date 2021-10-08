d43a43dc0727f4d84e98eac4f8d2f8db3d5622ee
Devin Rathnayake <54368055+devin989@users.noreply.github.com>
2021-10-08 20:10:46 +0530

    Create bluetooth_func.dart

---
+++ Dart-Programs/bluetooth_func.dart
@@ -0,0 +1,38 @@
+Future<void> initPlatformState() async {
+    bool isConnected = await bluetooth.isConnected;
+    List<BluetoothDevice> devices = [];
+    try {
+      devices = await bluetooth.getBondedDevices();
+    } on PlatformException {
+      // TODO - Error
+    }
+
+    bluetooth.onStateChanged().listen((state) {
+      switch (state) {
+        case BlueThermalPrinter.CONNECTED:
+          setState(() {
+            _connected = true;
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECTED:
+          setState(() {
+            _connected = false;
+          });
+          break;
+        default:
+          print(state);
+          break;
+      }
+    });
+
+    if (!mounted) return;
+    setState(() {
+      _devices = devices;
+    });
+
+    if (isConnected) {
+      setState(() {
+        _connected = true;
+      });
+    }
+  }
